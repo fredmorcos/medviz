7a218edfcb0d446067bb6ba400a541bc326f6a5c
Fred Morcos <fm@fredmorcos.com>
2021-05-09 13:43:33 +0200

    Add support for producing files containing raw frame data

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use derive_new::new;
 use log::{debug, info, trace};
 use medviz::{utils, Volume, VolumeErr, VolumeMd, VolumeMdErr};
 use memmap::MmapOptions;
-use std::fmt;
-use std::io;
+use std::io::{self, BufWriter};
 use std::num::TryFromIntError;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::{fmt, io::Write};
 use std::{fs::File, io::Read};
 use structopt::StructOpt;
 
@@ -47,6 +47,10 @@ struct Opt {
   /// Verbose output (can be specified multiple times).
   #[structopt(short, long, parse(from_occurrences))]
   verbose: u8,
+
+  /// Produce raw data instead of bmp images.
+  #[structopt(short, long)]
+  raw: bool,
 
   /// Input: Metadata file.
   #[structopt(short, long, name = "metadata-file", parse(from_os_str))]
@@ -99,23 +103,66 @@ fn main() -> Result<(), Err> {
 
   let volume = Volume::<u16>::from_slice(metadata, &map)?;
 
-  {
-    info!("Creating Y-frame (bmp)");
+  if opt.raw {
     // Produce the Y-frame, made up of voxels on the X- and Z-axis.
-    let yimage =
-      utils::frame_bmp(metadata.xdim(), metadata.zdim(), volume.yframe(metadata.ydim() / 2))?;
-    info!("Saving Y-frame (bmp) to {}", opt.yfile.display());
-    yimage.save(&opt.yfile)?;
+    create_frame_raw("Y-frame", &opt.yfile, volume.yframe(metadata.ydim() / 2))?;
+
+    // Produce the Z-frame, made up of voxels on the X- and Y-axis.
+    create_frame_raw("Z-frame", &opt.zfile, volume.zframe(metadata.zdim() / 2))?;
+  } else {
+    // Produce the Y-frame, made up of voxels on the X- and Z-axis.
+    create_frame_image(
+      "Y-frame",
+      &opt.yfile,
+      metadata.xdim(),
+      metadata.zdim(),
+      volume.yframe(metadata.ydim() / 2),
+    )?;
+
+    // Produce the Z-frame, made up of voxels on the X- and Y-axis.
+    create_frame_image(
+      "Z-frame",
+      &opt.zfile,
+      metadata.xdim(),
+      metadata.ydim(),
+      volume.zframe(metadata.zdim() / 2),
+    )?;
   }
 
-  {
-    info!("Creating Z-frame (bmp)");
-    // Produce the Z-frame, made up of voxels on the X- and Y-axis.
-    let zimage =
-      utils::frame_bmp(metadata.xdim(), metadata.ydim(), volume.zframe(metadata.zdim() / 2))?;
-    info!("Saving Z-frame (bmp) to {}", opt.zfile.display());
-    zimage.save(&opt.zfile)?;
+  Ok(())
+}
+
+/// Produce a file with raw contents of the selected frame.
+fn create_frame_raw(
+  frame_name: &'static str,
+  filename: &Path,
+  frame_iter: impl Iterator<Item = (u16, usize, usize)>,
+) -> Result<(), io::Error> {
+  info!("Creating {} (raw) file at {}", frame_name, filename.display());
+  let file = File::create(filename)?;
+  let mut writer = BufWriter::new(file);
+
+  info!("Writing {} (raw) to {}", frame_name, filename.display());
+  for (voxel, _, _) in frame_iter {
+    writer.write_all(&voxel.to_le_bytes())?;
   }
+
+  Ok(())
+}
+
+/// Produce a bmp image file of the selected frame.
+fn create_frame_image(
+  frame_name: &'static str,
+  filename: &Path,
+  dim1: usize,
+  dim2: usize,
+  frame_iter: impl Iterator<Item = (u16, usize, usize)>,
+) -> Result<(), Err> {
+  info!("Creating {} (bmp)", frame_name);
+  let image = utils::frame_bmp(dim1, dim2, frame_iter)?;
+
+  info!("Saving {} (bmp) to {}", frame_name, filename.display());
+  image.save(filename)?;
 
   Ok(())
 }
